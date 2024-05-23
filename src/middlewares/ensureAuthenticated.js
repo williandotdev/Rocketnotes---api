b1e4c0deba7e7d2a13bded37c0f024d208cc67b8
Willian <willian.dev.ferreira@gmail.com>
2024-05-23 18:17:53 -0300

    feat: upload img

--- src/middlewares/ensureAuthenticated.js
+++ src/middlewares/ensureAuthenticated.js
@@ -3,7 +3,7 @@ const AppError = require("../utils/AppError");
 const authConfig = require("../configs/auth");
 
 function ensureAuthenticated(request, response, next){
-    const authHeader = request.body.autorization;
+    const authHeader = request.headers.authorization;
 
     if(!authHeader){
         throw new AppError("JWT Token inválido", 401);
